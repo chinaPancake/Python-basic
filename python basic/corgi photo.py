14631372df1e5f5dc82f38954fde14ab79eed51b
chinaPancake <sitletto@gmail.com>
2022-06-22 23:51:47 +0200

    Right now I have random page from 1-350 (1 page = 1 photo on pexel) after that photo insted of link give me pop-up photo from webview lib.

--- python basic/corgi photo.py
+++ python basic/corgi photo.py
@@ -1,18 +1,27 @@
 from pexels_api import API
 from PIL import Image
+from tkinter import *
 import requests
+import random
+import webview
+
+rpage = random.randint(1,350)
 
 #wgranie api key z pexels
 PEXELS_API_KEY = '563492ad6f917000010000018b2652bcf7e841a0b049109d310692e0'
 api = API(PEXELS_API_KEY)
 
 #wyszukiwanie hasła 'corgi' z strony 1, 5 wyszukań na strone
-api.search('corgi', page=1, results_per_page=1)
+api.search('corgi', page=rpage, results_per_page=1)
 photos = api.get_entries()
 
 for photo in photos:
     print('photographer: ', photo.photographer)
     print(photo.original)
 
-im = Image.open(requests.get(url=photo.original, stream=True).raw)
-im.show()
+windows = Tk()
+webview.create_window('Corgi for Oliwia', photo.original)
+webview.start()
+
+#im = Image.open(requests.get(url=photo.original, stream=True).raw)
+#im.show()
