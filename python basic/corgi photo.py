98ef802b0e3e10d16d7b1359a9c896b03eb8b568
chinaPancake <sitletto@gmail.com>
2022-06-22 22:55:38 +0200

    I added PIL lib to open image from pexel URL.

--- python basic/corgi photo.py
+++ python basic/corgi photo.py
@@ -1,14 +1,18 @@
 from pexels_api import API
-from tkinter import *
+from PIL import Image
+import requests
 
 #wgranie api key z pexels
 PEXELS_API_KEY = '563492ad6f917000010000018b2652bcf7e841a0b049109d310692e0'
 api = API(PEXELS_API_KEY)
 
 #wyszukiwanie hasła 'corgi' z strony 1, 5 wyszukań na strone
-api.search('corgi', page=1, results_per_page=5)
+api.search('corgi', page=1, results_per_page=1)
 photos = api.get_entries()
 
 for photo in photos:
     print('photographer: ', photo.photographer)
     print(photo.original)
+
+im = Image.open(requests.get(url=photo.original, stream=True).raw)
+im.show()
