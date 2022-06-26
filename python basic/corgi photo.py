00ab4046907e217aa6e2c0143830408badf0c8a3
chinaPancake <sitletto@gmai.com>
2022-06-26 16:18:22 +0200

    I added to-do project

--- python basic/corgi photo.py
+++ python basic/corgi photo.py
@@ -5,9 +5,12 @@ import requests
 import random
 import webview
 
+windows = Tk()
+windows.geometry("1980x1640")
+
 rpage = random.randint(1,350)
 
-#wgranie api key z pexels
+ #wgranie api key z pexels
 PEXELS_API_KEY = '563492ad6f917000010000018b2652bcf7e841a0b049109d310692e0'
 api = API(PEXELS_API_KEY)
 
@@ -19,7 +22,6 @@ for photo in photos:
     print('photographer: ', photo.photographer)
     print(photo.original)
 
-windows = Tk()
 webview.create_window('Corgi for Oliwia', photo.original)
 webview.start()
 
---
+++ python basic/to-do.py
@@ -0,0 +1,94 @@
+import keyboard
+from keyboard import *
+from tkinter import *
+from tkinter import messagebox
+
+def newTask():
+    task = my_entry.get()
+    if task !='':
+        lb.insert(END, task)
+        my_entry.delete(0, 'end')
+    else:
+        messagebox.showwarning('warning', 'please enter some task.')
+def deleteTask():
+    lb.delete(ANCHOR)
+
+window = Tk()
+window.geometry('500x450+500+200')
+window.title('To-do list')
+window.config(bg='#223441')
+window.resizable(width=False, height=False)
+
+frame = Frame(window)
+frame.pack(pady=10)
+
+lb = Listbox(
+    frame,
+    width = 25,
+    height= 8,
+    font=('Times', 18),
+    bd = 0,
+    fg = '#464646',
+    highlightthickness = 0,
+    selectbackground = '#a6a6a6',
+    activestyle = 'none',
+)
+
+lb.pack(side=LEFT, fill=BOTH)
+
+task_list = [
+    'Eat apple',
+    'drink water',
+    'go gym',
+    'write software',
+    'write documentaion',
+    'take a nap',
+    'Learn smth',
+    'paint canvas'
+]
+
+for item in task_list:
+    lb.insert(END, item)
+
+sb = Scrollbar(frame)
+sb.pack(side=RIGHT, fill=BOTH)
+
+lb.config(yscrollcommand=sb.set)
+sb.config(command=lb.yview)
+
+my_entry = Entry(
+    window,
+    font=('times', 24)
+)
+my_entry.pack(pady=20)
+
+button_frame = Frame(window)
+button_frame.pack(pady=20)
+
+addTask_btn = Button(
+    button_frame,
+    text='Add task',
+    font=('times 14'),
+    bg='#c5f776',
+    padx=20,
+    pady=10,
+    command=newTask
+)
+addTask_btn.pack(fill=BOTH, expand=True, side=LEFT)
+
+delTask_btn = Button(
+    button_frame,
+    text='delete task',
+    font=('times 14'),
+    bg='#ff8b61',
+    padx=20,
+    pady=10,
+    command=deleteTask
+)
+delTask_btn.pack(fill=BOTH, expand=True, side=LEFT)
+
+if keyboard.is_pressed('enter'):
+    print('enter')
+    newTask()
+
+window.mainloop()
