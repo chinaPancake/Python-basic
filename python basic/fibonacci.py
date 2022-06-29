3ce01621a71f8d5ff555d15214c02c56d09f52bb
chinapancake <sitletto@gmail.com>
2022-06-29 20:56:56 +0200

    trying to do fibonnaci loop without googling

--- python basic/fibonacci.py
+++ python basic/fibonacci.py
@@ -1,7 +1,6 @@
-def fibonnaci(a):
-    while a > 10:
-        print(a)
-        a += 1
-
 a = int(input('podaj a: '))
+
+def fibonnaci(a):
+    for a in range (1,10):
+        print(a)
 print(fibonnaci(a))
